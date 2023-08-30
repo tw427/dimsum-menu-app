d952eaaf7dc0012a7ea5b0f5f0b97aefd8dba0ee
Tony W <bobacrypto2@gmail.com>
2023-08-30 02:19:16 -0700

    Update menu.js to include all our routes upon GET or POST methods for our categoryController and dishController functions

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,3 +4,36 @@ const asyncHandler = require("express-async-handler");
 exports.index = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Site Home Page");
 });
+
+// Display list of categories
+exports.category_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category list");
+});
+// Details for specific category
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: category detail ${req.params.id}`);
+});
+// Display category create form on GET
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category create GET");
+});
+// Handle category create on POST
+exports.category_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category create POST");
+});
+// Display category delete form on GET
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category delete GET");
+});
+// Handle category delete on POST
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category delete POST");
+});
+// Display category update form on GET
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category update GET");
+});
+// Handles category update POST
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category update POST");
+});
--- routes/menu.js
+++ routes/menu.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET menu listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+const category_controller = require("../controllers/categoryController");
+const dish_controller = require("../controllers/dishController");
+
+// CATEGORY ROUTES
+
+// GET menu Home page
+router.get("/", category_controller.index);
+
+// Create
+router.get("/category/create", category_controller.category_create_get);
+router.post("/category/create", category_controller.category_create_post);
+// Delete
+router.get("/category/:id/delete", category_controller.category_delete_get);
+router.post("/category/:id/delete", category_controller.category_delete_post);
+// Update
+router.get("/category/:id/update", category_controller.category_update_get);
+router.post("/category/:id/update", category_controller.category_update_post);
+
+// (Read) GET detail for a category
+router.get("/category/:id", category_controller.category_detail);
+
+// GET list for categories
+router.get("/categories", category_controller.category_list);
+
+// DISH ROUTES
+router.get("/dish/create", dish_controller.dish_create_get);
+router.post("/dish/create", dish_controller.dish_create_post);
+
+router.get("/dish/:id/delete", dish_controller.dish_delete_get);
+router.post("/dish/:id/delete", dish_controller.dish_delete_post);
+
+router.get("/dish/:id/update", dish_controller.dish_update_get);
+router.post("/dish/:id/update", dish_controller.dish_update_post);
+
+router.get("/dish/:id", dish_controller.dish_detail);
+
+router.get("/dishes", dish_controller.dish_list);
 
 module.exports = router;
